c7e2166469d9e624586565f1fc9d3d01d48b7056
MGX82 <rifky@kudo.co.id>
2017-05-28 21:54:51 +0700

    xx

---
+++ command.php
@@ -0,0 +1,4 @@
+1. php artisan make:middleware CheckAge => create middleware
+file : app/http/middleware/
+2. php artisan make:model Models/auth => create models
+3. php artisan make:controller huhuy => create Controller
